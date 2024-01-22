98df86202f23cb870365ce3e4ac0eee6c6cd4fe7
DjanGomes <dgt1000@gmail.com>
2024-01-22 20:46:02 -0300

    meme

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,23 +1,65 @@
-import React, { useState } from "react";
-import memesData from "../memesData.js";
+import React, { useEffect, useState } from "react";
+//import memesData from "../memesData.js";
 
 export default function Meme() {
-  const [memeUrl, setMemeUrl] = useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState("");
+
+  useEffect(() => {
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemeImages(data.data.memes);
+    }
+    getMemes();
+  }, []);
 
   let url;
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     url = memesArray[randomNumber].url;
-    setMemeUrl(url);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+
+    //setMemeUrl(url);
     //console.log(url);
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
   }
 
   return (
     <section className="section-entrada">
       <div className="div-entrada">
-        <input className="entrada-input" type="text" />
-        <input className="entrada-input" type="text" />
+        <input
+          className="entrada-input"
+          type="text"
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          className="entrada-input"
+          type="text"
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
       </div>
       <div className="div-btn">
         <button className="meme-btn" type="button" onClick={getMemeImage}>
@@ -25,7 +67,9 @@ export default function Meme() {
         </button>
       </div>
       <div className="div-img-aleatoria">
-        <img className="img-aleatoria" src={memeUrl} alt=""></img>
+        <img className="img-aleatoria" src={meme.randomImage} alt=""></img>
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
       </div>
     </section>
   );
